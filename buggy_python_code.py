4184e757c9aa1f180cd272a4453b791c4547b03f
unknown <xmikula1@ad.fi.muni.cz>
2025-04-28 13:07:52 +0200

    fixed 3 bugs

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,4 +1,3 @@
-import sys 
 import os
 import yaml
 import flask
@@ -30,7 +29,6 @@ def fetch_website(urllib_version, url):
  
     try: 
         http = urllib.PoolManager()
-        r = http.request('GET', url)
     except:
         print('Exception')
 
@@ -44,6 +42,7 @@ def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
